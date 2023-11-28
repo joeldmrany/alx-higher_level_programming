17ec8fbdb7246f969a86b4de1c14b99d030f29bc
joeldmrany <joeldmrany238@gmail.com>
2023-11-28 23:05:37 +0200

    11 task: 0

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def pow(a, b):
+    d = a
+    if b == 0:
+        return 1
+    elif b > 0:
+        for i in range(1, b):
+            if a < 0:
+                if (b % 2) == 0:
+                    c = d * a
+                    d = c
+                else:
+                    c = d * a
+                    d = c
+            else:
+                c = d * a
+                d = c
+        return c
+    else:
+        for i in range(b , -1):
+            c = d * a
+            d = c
+        c = 1 / c
+        return c
