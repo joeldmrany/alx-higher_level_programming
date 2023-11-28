7f4c645af659963612ddac6caf47a6b7485b9adb
joeldmrany <joeldmrany238@gmail.com>
2023-11-28 21:33:00 +0200

    8 task: 1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        print("{}".format(i.upper()), end='')
+        print("{}".format((chr(ord(i)-32)) if ord(i) <= ord('z') and ord(i) >= ord('a') else i), end='')
     print("")
