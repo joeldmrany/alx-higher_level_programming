e918491a69e1f239e448f64d5ca9764ef9168aca
joeldmrany <joeldmrany238@gmail.com>
2023-11-30 16:24:12 +0200

    task: 2, attempt: 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     elif len(arg) == 2:
         print("1 argument:")
     else:
-        print("{} argument:".format(a - 1))
+        print("{} arguments:".format(a - 1))
     for i in range(len(arg)):
         if i == 0:
             continue
