b93c1ed14bd4bbd4463663140e5db365796502d5
joeldmrany <joeldmrany238@gmail.com>
2023-12-04 19:20:49 +0200

    task: 9, attempt: 0

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    b = my_list[0]
+    for i in range(len(my_list) - 1):
+        if (not my_list):
+            b = None
+        else:
+            if b <= my_list[i]:
+                b = my_list[i]
+    return b
