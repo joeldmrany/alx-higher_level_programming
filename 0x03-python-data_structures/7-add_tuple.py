dc4611802a3ce55c90d8f154af53d0e1f54b3234
joeldmrany <joeldmrany238@gmail.com>
2023-12-04 11:48:30 +0200

    task: 7, attempt: 0

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ()
-    if (len(tuple_a)) == 2 and (len(tuple_b)) == 2:
-        for i in range(len(tuple_a)):
-            tuple_c(i) = int(tuple_a(i)) + int(tuple_b(i))
-        return tuple_c
+    if (((len(tuple_a)) == 2 and (len(tuple_b)) == 2) or ((len(tuple_a) > 2) and (len(tuple_b) > 2))):
+        a = int(tuple_a[0]) + int(tuple_b[0])
+        b = int(tuple_a[1]) + int(tuple_b[1])
+    elif ((len(tuple_a)) == 1) and (len(tuple_b) == 2):
+        a = int(tuple_a[0]) + int(tuple_b[0])
+        b = (0) + int(tuple_b[1])
+    elif ((len(tuple_a)) == 2) and (len(tuple_b) == 1):
+        a = int(tuple_a[0]) + int(tuple_b[0])
+        b = int(tuple_a[1]) + (0)
+    elif ((len(tuple_a)) == 0) and (len(tuple_b) == 2):
+        a = (0) + int(tuple_b[0])
+        b = (0) + int(tuple_b[1])
+    elif ((len(tuple_a)) == 2) and (len(tuple_b) == 0):
+        a = int(tuple_a[0]) + (0)
+        b = int(tuple_a[1]) + (0)
+    elif ((len(tuple_a)) == 1) and (len(tuple_b) == 0):
+        a = int(tuple_a[0]) + (0)
+        b = 0
+    elif ((len(tuple_a)) == 0) and (len(tuple_b) == 1):
+        a = (0) + int(tuple_b[0])
+        b = (0)
+    elif ((len(tuple_a)) == 1) and (len(tuple_b) == 2):
+        a = 0
+        b = 0
+    tuple_c = (a, b)
+    return tuple_c
