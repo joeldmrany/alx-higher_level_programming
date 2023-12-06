be622b3fa0c09ccd9abaf50bb5f134b9718349de
joeldmrany <joeldmrany238@gmail.com>
2023-12-06 16:36:04 +0200

    task: 1, attempt: 0

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == 2:
-            my_list[i] = 89
-        else:
-            pass
-    return my_list
+    return [89 if x == 2 else x for x in my_list]
