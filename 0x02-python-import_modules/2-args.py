36b5a83ae3bd456394f0f823b99c40b22adeef49
joeldmrany <joeldmrany1@gmail.com>
2024-03-14 11:44:38 +0200

    task: 2, attempt: 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments".format(num - 1))
+        print("{} arguments:".format(num - 1))
         for i in range(1, num):
             print("{}: {}".format(i, argv[i]))
