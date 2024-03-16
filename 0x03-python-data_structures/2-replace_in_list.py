4cc91ae5095177b013dfc3d693a82aa9b1fa7712
joeldmrany <joeldmrany1@gmail.com>
2024-03-16 23:06:50 +0200

    edit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,5 +7,4 @@ def replace_in_list(my_list, idx, element):
         return None
     else:
         my_list[idx] = element
-        new_list = my_list
-        return(new_list)
+        return my_list
