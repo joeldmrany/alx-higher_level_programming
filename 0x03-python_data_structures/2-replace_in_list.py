e847c61ed8294f23ea3bd76c3678528b7709f949
joeldmrany <joeldmrany1@gmail.com>
2024-03-16 22:47:28 +0200

    task: 2, attempt: 0

---
+++ 0x03-python_data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    num = len(my_list)
+    if idx > num:
+        return None
+    elif idx < 0:
+        return None
+    else:
+        my_list[idx] = element
+        return(my_list)
