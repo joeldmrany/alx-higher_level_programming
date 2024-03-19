5de07d7f91cefe885efcf00ecf899ef4319a2972
joeldmrany <joeldmrany1@gmail.com>
2024-03-19 14:51:57 +0200

    task: 8, attempt: 2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence or (sentence == ""):
+    if not sentence:
         tuple_a = (0, None)
     else:
         i = 0
         for num in sentence:
             i = i + 1
         tuple_a = (i, sentence[0])
-        return tuple_a
+    return tuple_a
