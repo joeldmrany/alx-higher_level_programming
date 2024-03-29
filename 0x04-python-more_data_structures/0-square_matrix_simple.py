9623930290098d08f62b560c96afd80d5c0518f0
joeldmrany <joeldmrany1@gmail.com>
2024-03-29 22:28:44 +0200

    task: 0, attempt: 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    new = [row[:] for row in matrix]
+    for i in range(len(matrix)):
+        for x in range(len(matrix)):
+            new[i][x] = ((new[i][x])**2)
+    return new
