363efaaa3295620f077a7a1f38f99deb282e4c38
joeldmrany <joeldmrany1@gmail.com>
2024-06-19 03:17:29 +0300

    task: 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 2) {
+if (!args[2]) {
   console.log('No argument');
-} else if (args.length > 2) {
+} else {
   console.log(args[2]);
 }
