a7aaf9d8c101e11573cacc7ac4e375dd81cc0a5d
joeldmrany <joeldmrany1@gmail.com>
2024-06-19 03:43:10 +0300

    task: 5'

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 const num = parseInt(args[2]);
-if (!args[2]) {
+if (!args[2] || isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + num);
