8071ce1fbc722ac389cc04b778922b6734b36530
joeldmrany <joeldmrany1@gmail.com>
2024-06-30 20:41:25 +0300

    task: 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define Square class """
+
+
+class Square:
+    """ that is empty class which is named by Square """
+    def __init__(self, size):
+        """ it takes arg size """
+        self.__size = size
