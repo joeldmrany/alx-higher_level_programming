d3647eb1a19892e699635210d6d7ac82f2bc487b
joeldmrany <joeldmrany1@gmail.com>
2024-06-30 20:52:10 +0300

    Task: 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" define Square class """
+
+
+class Square:
+    """ that is empty class which is named by Square """
+    def __init__(self, size=0):
+        """ it takes arg size """
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ function to calculate area of square """
+        return (self.__size * self.__size)
