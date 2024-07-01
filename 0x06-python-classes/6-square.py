7fb55a6aecd878fa981268b455c47183db19ec8c
joeldmrany <joeldmrany1@gmail.com>
2024-07-01 17:40:37 +0300

    task: 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,9 @@ class Square:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
-        if (not isinstance(position, tuple) or len(position) != 2 or position[1] < 0 or position[0] < 0):
+        if (not isinstance(position, tuple) or len(position) != 2 or
+                all(num < 0 for num in position) or
+                not all(isinstance(num, int)for num in position)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
