c5e5069bfa810f0f3131b956dc5faa8744eb7d83
joeldmrany <joeldmrany1@gmail.com>
2024-08-02 20:32:05 +0300

    task: 1'

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,17 +4,22 @@ That is a function to divide matrix by number
 Try
 Don't forget your feedback
 """
+
+
 def matrix_divided(matrix, div):
     """
     that is matrix_divided function
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
     for row in matrix:
         if not all(isinstance(item, (int, float)) for item in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/floats")
     length = len(matrix[0])
     if not all(len(row) == length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
