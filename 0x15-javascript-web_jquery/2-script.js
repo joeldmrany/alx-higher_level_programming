fee5edd087986b5d2e92c0dbdded94786f659356
joeldmrany <joeldmrany1@gmail.com>
2024-08-08 20:46:06 +0300

    task: 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const redHeaderDiv = document.querySelector("#red_header");
+    const header = document.querySelector("header");
+
+    redHeaderDiv.addEventListener("click", function() {
+        header.style.color = "#FF0000";
+    });
+});
+
