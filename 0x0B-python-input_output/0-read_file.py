88c7584d556d63d7d47358a24505e1955e031e80
joeldmrany <joeldmrany1@gmail.com>
2024-08-09 19:21:34 +0300

    task: 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ function to read file content
 
 
 def read_file(filename=""):
+    """ that is the function """
     with open(filename, "r", encoding="UTF-8") as file:
         content = file.read()
         return (content)
