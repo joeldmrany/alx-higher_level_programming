be016a7f2237092da9aa672deedc5e9524c04d88
joeldmrany <joeldmrany1@gmail.com>
2024-09-17 00:01:54 +0300

    Mysql

---
+++ 0x0E-SQL_more_queries/2-creat_read_user.sql
@@ -0,0 +1,11 @@
+-- Create the database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user only if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database hbtn_0d_2 user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+
+-- Apply the privilege changes
+FLUSH PRIVILEGES;
